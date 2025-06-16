4b0d850f65b2b4a56bc95bc291d080dfa157644c
trinhkhanhltc <trinhkhanhltc@gmail.com>
2025-06-16 14:42:09 +0700

    feature/getDetail-use-PathVariable

--- src/main/java/com/khanhtt/demospringboot/controller/UserController.java
+++ src/main/java/com/khanhtt/demospringboot/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.khanhtt.demospringboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,11 @@ public class UserController {
   @GetMapping("/add")
   public String addUser(){
     return "Hello Add User";
+  }
+
+  @GetMapping("/getDetail/{id}")
+  public String getDetail(@PathVariable("id") int id)
+  {
+    return "Hello " + id;
   }
 }
