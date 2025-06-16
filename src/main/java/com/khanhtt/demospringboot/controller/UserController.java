6a7d02bad53b47394b965967592c364a385dac3c
trinhkhanhltc <trinhkhanhltc@gmail.com>
2025-06-16 10:54:52 +0700

    add a GET feature

---
+++ src/main/java/com/khanhtt/demospringboot/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.khanhtt.demospringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+  @GetMapping("/getAllUser")
+  public String getAllUser()
+  {
+    return "Hello World";
+  }
+}
