89ac2eca80f6322803fc2aa095f8a316ba4b9c7c
trinhkhanhltc <trinhkhanhltc@gmail.com>
2025-06-17 09:32:41 +0700

    feature/add update function

--- src/main/java/com/khanhtt/demospringboot/controller/UserController.java
+++ src/main/java/com/khanhtt/demospringboot/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.khanhtt.demospringboot.controller;
 
+import com.khanhtt.demospringboot.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +22,15 @@ public class UserController {
     return "Hello Add User";
   }
 
-  @GetMapping("/getDetail/{id}")
-  public String getDetail(@PathVariable("id") int id)
+  @GetMapping("/getDetail/{id}/{name}")
+  public String getDetail(@PathVariable("id") int id, @PathVariable("name") String name)
   {
-    return "Hello " + id;
+    return "Hello " + id + " " + name;
+  }
+
+  @PostMapping("/update")
+  public String update(@RequestBody User user)
+  {
+    return "Hello " + user.getId() + " - " + user.getName() + " - " + user.getPassword();
   }
 }
---
+++ src/main/java/com/khanhtt/demospringboot/model/User.java
@@ -0,0 +1,10 @@
+package com.khanhtt.demospringboot.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+  private String id;
+  private String name;
+  private String password;
+}
